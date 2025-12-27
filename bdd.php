1f02543b7d488393d7a220c5edff46271be0cb62
Stella Elian <elian_stella@yahoo.com>
2025-12-27 16:28:23 +0100

    commentaires+correction dbname=

--- bdd.php
+++ bdd.php
@@ -2,7 +2,7 @@
 function connexion()
 {
     try{
-        return new PDO('mysql:host=localhost;dbname=artbox;charset=utf8','root','');
+        return new PDO('mysql:host=localhost;dbname=artbox;charset=utf8','root',''); // attention à la configuration qui est que les différents paramètres doivent prendre une valeur donc un signe = 
     }
     catch(Exception $e)
     {
