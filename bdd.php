c2f225854087970c8061872a841aac2e14f5e059
Stella Elian <elian_stella@yahoo.com>
2025-12-27 15:33:42 +0100

    création du fichier bdd de connexion à la base de données

--- bdd.php
+++ bdd.php
@@ -2,8 +2,7 @@
 function connexion()
 {
     try{
-        return new PDO('mysql:host=localhost;dbname:artbox;charset:utf8','root','');
-
+        return new PDO('mysql:host=localhost;dbname:artbox;charset=utf8','root','');
     }
     catch(Exception $e)
     {
