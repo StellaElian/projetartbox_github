4de5fbf3db02c31783cb4f7674ee72657b034967
Stella Elian <elian_stella@yahoo.com>
2025-12-27 15:37:41 +0100

    Remplacement de oeuvre par o dans le foreach

--- oeuvre.php
+++ oeuvre.php
@@ -10,7 +10,8 @@
     $oeuvre = null;
 
     // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
+    foreach($oeuvres as $o)  
+        {
         // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
         if($o['id'] === intval($_GET['id'])) {
             $oeuvre = $o;
