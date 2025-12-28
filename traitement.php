39700781e5624b8c407f2af90351edffebc077aa
Stella Elian <elian_stella@yahoo.com>
2025-12-28 23:08:06 +0100

    validation du formulaire de création d'une oeuvre

--- traitement.php
+++ traitement.php
@@ -1,10 +1,11 @@
 <?php
 
-if(empty($_POST['titre'])||empty($_POST['artiste'])||empty($_POST['description'])||empty($_POST['image'])|| strlen($_POST['description'])<3 || filter_var($_POST['image'],FILTER_VALIDATE_URL)
+if(empty($_POST['titre'])|| empty($_POST['artiste']) || empty($_POST['description']) || empty($_POST['image']) || strlen($_POST['description']) <3 || !filter_var($_POST['image'],FILTER_VALIDATE_URL)
     )
 {
     header('Location : ajouter.php');
-}else {
+}
+else {
 
     titre=htmlspecialchars($_POST[titre]);
     artiste=htmlspecialchars($_POST[artiste]);
