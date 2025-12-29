bc2e509725bb87eeee4f5b0ae93e7b6e1ceba1c3
Stella Elian <elian_stella@yahoo.com>
2025-12-29 03:06:31 +0100

    correction erreur bdd, desciptionet image  et un ou en plus dans mon if

--- traitement.php
+++ traitement.php
@@ -1,7 +1,8 @@
 <?php
 
-if(empty($_POST['titre'])|| empty($_POST['artiste']) 
-    || empty($_POST['description']) || empty($_POST['image']) 
+if(empty($_POST['titre'])|| empty($_POST['description'])
+    || empty($_POST['artiste']) 
+    || empty($_POST['image']) 
     || strlen($_POST['description']) < 3 
     || !filter_var($_POST['image'],FILTER_VALIDATE_URL)
     )
@@ -11,14 +12,14 @@ if(empty($_POST['titre'])|| empty($_POST['artiste'])
 else {
 
     $titre= htmlspecialchars($_POST[titre]);
-    $artiste= htmlspecialchars($_POST[artiste]);
     $description= htmlspecialchars($_POST[description]);
+    $artiste= htmlspecialchars($_POST[artiste]);
     $image= htmlspecialchars($_POST[image]);
 
     require 'bdd.php' ;
-    $db=connexion();
-    $requete=$db->prepare('INSERT INTO Oeuvres (titre,artiste,description,image)VALUES (?, ?, ?, ?)');
-    $requete=execute([$titre, $artiste, $description, $image]); // renseigner les variables
+    $bdd=connexion();
+    $requete=$bdd->prepare('INSERT INTO oeuvres (titre, description, artiste, image)VALUES (?, ?, ?, ?)');
+    $requete=execute([$titre, $description, $artiste,  $image]); // renseigner les variables
 
     // Il faut rediriger l'utilisateur vers la page de la liste des oeuvres 
 
