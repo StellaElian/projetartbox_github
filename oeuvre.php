0e94d2e212ee7f7ee91b66a58de6d5cbad565fd7
Stella Elian <elian_stella@yahoo.com>
2025-12-29 03:20:31 +0100

    correction totale car c'etait pas dans le if + espace après le signe égale

--- oeuvre.php
+++ oeuvre.php
@@ -1,33 +1,21 @@
 <?php
     require 'header.php';
-    //require 'oeuvres.php';
     require 'bdd.php';
-    $bdd=connexion();
-    $requete=$bdd->prepare('SElECT * FROM oeuvres WHERE id = ?');
-    // il faut un GET pour récupérer l'id que l'utilisateur a choisi
-    $id= $_GET['id'];
-    $requete->execute([$id]);
-    $oeuvre=$requete->fetch(); //Pour récupérer l'oeuvre depuis la bd
+    $bdd= connexion();
 
+   
 
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
-    }
+    } 
+    $requete= $bdd->prepare('SElECT * FROM oeuvres WHERE id = ?');
+    // il faut un GET pour récupérer l'id que l'utilisateur a choisi
+    $requete->execute([$_GET['id']]);
+    $oeuvre=$requete->fetch(); //Pour récupérer l'oeuvre depuis la bd
 
-    $oeuvre = null;
 
-    //On l'efface car ne nous sert plus à rien , perso je le mets en commentaire pour comme même garder une trace 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    /*foreach($oeuvres as $o)  
-        {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }*/
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
     if(is_null($oeuvre)) {
